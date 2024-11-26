c49dd67484991cd68219d79ba4ae5d850065cad9
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-25 23:36:54 -0300

    feat: create option to select specific action

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import streamlit as st
 import yfinance as yf
 
-# ENBR3.SA - EDP Energias do Brasil S.A
 # BBAS3.SA - Banco do Brasil S.A
-# BBDCA.SA - Banco Bradesco S.A
 # PETR4.SA - Petroleo Brasileiro S.A - Petrobrás
-# VALE5.SA - VALE5.SA
 
 st.title("Preço de ativo")
 st.header("Informações a Respeito do Fechamento e Volume de Alumas Ações")
 
-ticker_symbol = "PETR4.SA"
+options = st.selectbox(
+    "Escolha o Ativo",
+    (
+        "BBAS3.SA",
+        "PETR4.SA",
+    ),
+)
+
+ticker_symbol = options
 ticker_data = yf.Ticker(ticker_symbol)
 ticker_data_frame = ticker_data.history(period="1d", start="2012-5-19", end="2022-5-19")
 
